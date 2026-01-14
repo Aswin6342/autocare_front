16f0825a5aa201f29a7d06094661f1ead3109b0c
Aswin6342 <aswinedakulathil@gmail.com>
2026-01-14 22:17:29 +0530

    link change

--- src/API/api.js
+++ src/API/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 //const API_URL = "http://localhost:5000";
-// const API_URL = "https://autocare-full.onrender.com"
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://autocare-full.onrender.com"
+// const API_URL = import.meta.env.VITE_API_URL;
 
 
 /* ================= AXIOS INSTANCE ================= */
